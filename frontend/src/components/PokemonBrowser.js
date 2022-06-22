bfc10074d784a2022fc57d21a73a41e08ed606f3
jkintner25 <jkintner25@gmail.com>
2022-06-22 17:11:47 -0500

    finished phase one

--- frontend/src/components/PokemonBrowser.js
+++ frontend/src/components/PokemonBrowser.js
@@ -1,10 +1,11 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Route, useParams } from 'react-router-dom';
 
 import PokemonDetail from './PokemonDetail';
 import CreatePokemonForm from './CreatePokemonForm';
 import Fab from './Fab';
+import { getPokemon } from '../store/pokemon';
 
 const PokemonBrowser = () => {
   const { pokemonId } = useParams();
@@ -13,6 +14,11 @@ const PokemonBrowser = () => {
   });
   const [showForm, setShowForm] = useState(false);
 
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(getPokemon())
+  }, [dispatch])
 
   if (!pokemon) {
     return null;
